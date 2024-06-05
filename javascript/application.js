0d3677c711e8022776fd3c7068e6735333edc4c9
Enrique Ramirez <nrqrmz@gmail.com>
2024-06-05 10:01:48 -0500

    vue

--- javascript/application.js
+++ javascript/application.js
@@ -1,24 +1,10 @@
-import Mustache from "mustachejs";
+import { createApp, ref } from "vue";
 
-const url = "https://www.omdbapi.com/?s=harry%20potter&apikey=adf1f2d7";
-const list = document.querySelector('#results');
-const template = document.querySelector('#movieCardTemplate').innerHTML;
-
-fetch(url)
-  .then(response => response.json())
-  .then((data) => {
-    const output = Mustache.render(template, { movies: data.Search });
-
-    list.innerHTML = output;
-  })
-
-// Mustache.render(string, object)
-// const output = Mustache.render('<h1>{{title}}</h1>', { title: 'Hello Mustache.js' });
-// list.insertAdjacentHTML('beforeend', output)
-
-// Template creation
-// Data input
-// Template engine
-// Output generation
-
-// Mustache is a class
+createApp({
+  data() {
+    return {
+      message: "Hello from Vue.JS",
+      count: ref(0)
+    }
+  }
+}).mount('#results')
